52cca68f2bbeeb23680ee7dc3dc32adae8425d8b
Farizzz <113644816+FarizzzAPK@users.noreply.github.com>
2025-02-08 17:34:31 +0200

    morning azkar

---
+++ lib/home/home.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:islami_c13_sunday/home/bottom_nav_bar.dart';
+import 'package:islami_c13_sunday/home/tabs/ahadeth/ahadeth_tab.dart';
+import 'package:islami_c13_sunday/home/tabs/date_tab/date_tab.dart';
+import 'package:islami_c13_sunday/home/tabs/quranTab/quran_tab.dart';
+import 'package:islami_c13_sunday/home/tabs/Radio_Reciters_list/radio_tab.dart';
+import 'package:islami_c13_sunday/home/tabs/sebha_tab.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const String routeName = "Home";
+
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/images/${getBackgroundImageName()}.png"),
+            fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        bottomNavigationBar: BottomNavBar(onChange: (index) {
+          currentIndex = index;
+          setState(() {});
+        }),
+        body: Column(
+          children: [
+            Image.asset("assets/images/onboarding_header.png"),
+            Expanded(child: tabs[currentIndex]),
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<Widget> tabs = [
+    QuranTab(),
+     AhadethTab(),
+    const SebhaTab(),
+     RadioTab(),
+    const DateTab()
+  ];
+
+  String getBackgroundImageName() {
+    switch (currentIndex) {
+      case 0:
+        return "home_bg";
+      case 1:
+        return "ahadeth_bg";
+      case 2:
+        return "sebha_bg";
+      case 3:
+        return "radio_bg";
+
+      default:
+        return "date_bg";
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:islami_c13_sunday/cache/cache_helper.dart';
+import 'package:islami_c13_sunday/hadeth_details/hadeth_details.dart';
+import 'package:islami_c13_sunday/home/home.dart';
+import 'package:islami_c13_sunday/my_theme_data.dart';
+import 'package:islami_c13_sunday/onboarding_screen.dart';
+import 'package:islami_c13_sunday/sura_details/sura_details.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await CacheHelper.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: MaterialApp(
+        theme: MyThemeData.lightTheme,
+        darkTheme: MyThemeData.darkTheme,
+        themeMode: ThemeMode.light,
+        debugShowCheckedModeBanner: false,
+        initialRoute: CacheHelper.getEligibility() == true
+            ? HomeScreen.routeName
+            : OnboardingScreen.routeName,
+        routes: {
+          OnboardingScreen.routeName: (context) => const OnboardingScreen(),
+          SuraDetailsScreen.routeName: (context) => SuraDetailsScreen(),
+          HadethDetailsScreen.routeName: (context) => const HadethDetailsScreen(),
+          HomeScreen.routeName: (context) => const HomeScreen(),
+        },
+      ),
+    );
+  }
+}
